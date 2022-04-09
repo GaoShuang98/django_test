8b5b798a6bb30b9d19efebc128a505fc38671191
GaoShuang98 <huanggaoshuang123@gmail.com>
2022-04-09 16:57:56 +0800

    增加触发alert弹窗

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,17 @@
 function fly2school(viewer) {
-   viewer.camera.flyTo({
-                        destination: Cesium.Cartesian3.fromDegrees(113.570, 34.8125, 3500.0),
-                    });
+    viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(113.570, 34.8125, 3500.0),
+    });
 }
 
-//
-// function import_img{
-//
-// }
+function import_img() {
+    alert("导入图像");
+}
+
+function img_positioning() {
+    alert("图像定位");
+}
+
+function myFunction() {
+    alert("你好，我是一个警告框！");
+}
