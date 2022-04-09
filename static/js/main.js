a26b619ab5637a1459d17a4f3ad600e80367e7cb
GaoShuang98 <huanggaoshuang123@gmail.com>
2022-04-09 15:31:34 +0800

    重新创建django工程

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+function fly2school(viewer) {
+   viewer.camera.flyTo({
+                        destination: Cesium.Cartesian3.fromDegrees(113.570, 34.8125, 3500.0),
+                    });
+}
+
+//
+// function import_img{
+//
+// }
