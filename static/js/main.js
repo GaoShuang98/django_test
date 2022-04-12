3707ae4e9e0a2fd7de80bd8363b04db82569d849
GaoShuang98 <huanggaoshuang123@gmail.com>
2022-04-12 18:21:24 +0800

    增加子界面

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,41 @@
-function fly2school(viewer) {
 
+function fly2school(viewer) {
+    var element = document.getElementById("img_positioning");
+    element.style.display = 'none'
     viewer.camera.flyTo({
         destination: Cesium.Cartesian3.fromDegrees(113.570, 34.8125, 3500.0),
     });
 }
 
-function main_window(){
-
-}
 
 function import_img() {
     alert("导入图像");
-
 }
 
 function img_positioning() {
-    window.location.replace("http://127.0.0.1:8000/import_img");
+    var element = document.getElementById("img_positioning");
     alert("图像定位");
+    element.style.display = 'block'
+
 }
 
 function target_positioning() {
-    window.location.replace("http://127.0.0.1:8000/target_positioning");
+    var element = document.getElementById("img_positioning");
     alert("目标定位");
+    element.style.display = 'none'
+
 }
 
 function monitoring() {
+    var element = document.getElementById("img_positioning");
     alert("舆情监测");
+    element.style.display = 'none'
+
 }
 
 function situational_awareness() {
+    var element = document.getElementById("img_positioning");
     alert("势态感知");
+    element.style.display = 'none'
+
 }
