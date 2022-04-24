60956d9f37afc7eca192413a0b9e5705c5bd3212
GaoShuang98 <huanggaoshuang123@gmail.com>
2022-04-24 22:22:52 +0800

    解决ID冲突问题

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
-var son_page_name = 'none'
+let son_page_name = 'none'
 
 function fly2school(viewer) {
-    var element = document.getElementById("img_positioning");
+    let element = document.getElementById("img_positioning");
     element.style.display = 'none'
     viewer.camera.flyTo({
         destination: Cesium.Cartesian3.fromDegrees(113.570, 34.8125, 3500.0),
@@ -17,7 +17,7 @@ function import_img() {
 }
 
 function img_positioning() {
-    var img_positioning = document.getElementById("img_positioning");
+    let img_positioning = document.getElementById("img_positioning");
     if (son_page_name === "img_positioning") {
         img_positioning.style.display = 'none';
         son_page_name = 'none';
@@ -25,7 +25,7 @@ function img_positioning() {
         img_positioning.style.display = 'block';
         son_page_name = 'img_positioning';
     } else {
-        var element = document.getElementById(son_page_name);
+        let element = document.getElementById(son_page_name);
         element.style.display = 'none';
         img_positioning.style.display = 'block';
         son_page_name = 'img_positioning';
@@ -33,7 +33,7 @@ function img_positioning() {
 }
 
 function target_positioning() {
-    var target_positioning = document.getElementById("target_positioning");
+    let target_positioning = document.getElementById("target_positioning");
     if (son_page_name === "target_positioning") {
         target_positioning.style.display = 'none';
         son_page_name = 'none';
@@ -41,7 +41,7 @@ function target_positioning() {
         target_positioning.style.display = 'block';
         son_page_name = 'target_positioning';
     } else {
-        var element = document.getElementById(son_page_name);
+        let element = document.getElementById(son_page_name);
         element.style.display = 'none';
         target_positioning.style.display = 'block';
         son_page_name = 'target_positioning';
@@ -49,7 +49,7 @@ function target_positioning() {
 }
 
 function monitoring() {
-    var monitoring = document.getElementById("monitoring");
+    let monitoring = document.getElementById("monitoring");
     if (son_page_name === "monitoring") {
         monitoring.style.display = 'none';
         son_page_name = 'none';
@@ -57,7 +57,7 @@ function monitoring() {
         monitoring.style.display = 'block';
         son_page_name = 'monitoring';
     } else {
-        var element = document.getElementById(son_page_name);
+        let element = document.getElementById(son_page_name);
         element.style.display = 'none';
         monitoring.style.display = 'block';
         son_page_name = 'monitoring';
@@ -65,7 +65,7 @@ function monitoring() {
 }
 
 function situation_awareness() {
-    var situation_awareness = document.getElementById("situation_awareness");
+    let situation_awareness = document.getElementById("situation_awareness");
     if (son_page_name === "situation_awareness") {
         situation_awareness.style.display = 'none';
         son_page_name = 'none';
@@ -73,7 +73,7 @@ function situation_awareness() {
         situation_awareness.style.display = 'block';
         son_page_name = 'situation_awareness';
     } else {
-        var element = document.getElementById(son_page_name);
+        let element = document.getElementById(son_page_name);
         element.style.display = 'none';
         situation_awareness.style.display = 'block';
         son_page_name = 'situation_awareness';
@@ -81,19 +81,19 @@ function situation_awareness() {
 }
 
 function cesium_container_clicked(){
-    var a = document.getElementById('dropdown-menu-localization');
+    let a = document.getElementById('dropdown-menu-localization');
     a.style.display = 'none';
     if (son_page_name === "none") {
         return 0;
     } else {
-        var e = document.getElementById(son_page_name);
+        let e = document.getElementById(son_page_name);
         e.style.display = 'none';
         son_page_name = 'none';
     }
 }
 
 function menu_localization_clicked(){
-    var e = document.getElementById('dropdown-menu-localization');
+    let e = document.getElementById('dropdown-menu-localization');
     if (e.style.display === 'block'){
         e.style.display = 'none';
     } else {
@@ -103,10 +103,10 @@ function menu_localization_clicked(){
 
 function file_import() {
     document.getElementById("files").click()
-    var selected_file = document.getElementById("files").files[0];  // 弹窗获取文件
-    var file_name = selected_file.name;  // 读取文件名
-    var file_size = selected_file.size;  // 获取文件大小
-    var file_dir = selected_file.directory;
+    let selected_file = document.getElementById("files").files[0];  // 弹窗获取文件
+    let file_name = selected_file.name;  // 读取文件名
+    let file_size = selected_file.size;  // 获取文件大小
+    let file_dir = selected_file.directory;
     alert(file_name + " // " + file_size + " // " + file_dir);
 }
 
